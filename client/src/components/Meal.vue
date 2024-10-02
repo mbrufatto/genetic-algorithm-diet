6ae49ecf6e61bc93747da55c1cb81ed99bb05375
Michael Marques <michaelycus@gmail.com>
2024-10-02 16:10:56 -0300

    Ajustes no layout

--- client/src/components/Meal.vue
+++ client/src/components/Meal.vue
@@ -15,57 +15,113 @@ function getGeneratedDiet() {
   axios.get('http://127.0.0.1:5000/get-diet')
     .then((response) => {
       this.diet = response.data;
-      console.log(this.diet);
     })
+}
+
+function postGeneratedDiet() {
+  axios.post('http://127.0.0.1:5000/post-diet', {
+    porcoes: props.config.target.servings,
+    calorias: props.config.target.calories,
+    proteinas: props.config.target.proteins,
+    lipidios: props.config.target.lipids,
+    carboidratos: props.config.target.carbohydrates,
+    fibras: props.config.target.fibers,
+    categorias: 4 // ?
+  })
+    .then((response) => {
+      this.diet = response.data;
+    })
+}
+
+function formatDecimal(value, digits) {
+    return value.toLocaleString("pt-BR", {
+        minimumFractionDigits: digits,
+        maximumFractionDigits: digits,
+    });
 }
 
 </script>
 
 <template>
-  <div class="flex items-center p-2 border rounded border-gray-300 text-gray-800">
-    <div class="min-w-32 font-semibold">
+  <div class="flex flex-col md:flex-row items-center space-x-2 border rounded border-gray-300 text-gray-800">
+
+    <!-- Título -->
+    <div class="min-w-32 font-semibold w-full md:w-1/12 text-center">
         {{ config.title }}
     </div>
-    <div class="flex flex-col space-y-2">
+
+    <!-- Configurações -->
+    <div class="flex flex-col space-y-2 w-full md:w-6/12 my-4">
         <FoodTypes :types-selected="config.typesSelected"></FoodTypes>
         <Target :target="config.target"></Target>
     </div>
 
-    <div v-if="diet != null" class="flex flex-col relative overflow-x-auto w-1/3 min-w-[500px] items-center">
-      <table class="w-full text-sm text-left rtl:text-right text-gray-500">
-        <thead class="text-xs text-gray-700 bg-gray-50">
-          <tr>
-            <th>Alimento</th>
-            <th>Categoria</th>
-            <th>Calorias (kcal)</th>
-            <th>Proteínas (g)</th>
-            <th>Lipidios (g)</th>
-            <th>Carboidratos (g)</th>
-            <th>Fibra (g)</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="food in diet" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50  border-b">
-            <td>{{ food.name }}</td>
-            <td>{{ food.category }}</td>
-            <td>{{ food.energy_kcal }}</td>
-            <td>{{ food.protein_g }}</td>
-            <td>{{ food.lipids_g }}</td>
-            <td>{{ food.carbohydrate_g }}</td>
-            <td>{{ food.dietary_fiber_g }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <div class="p-1">
+    <!-- Resultado -->
+    <div class="w-full md:w-5/12">
+
+      <!-- Tabela Resultado -->
+      <div v-if="diet != null" class="flex flex-col relative overflow-x-auto items-center">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 font-mono">
+          <thead class="text-xs text-gray-700 bg-gray-50">
+            <tr>
+              <th class="text-center">Alimento</th>
+              <th class="text-center">Categoria</th>
+              <th class="text-right">Calorias (kcal)</th>
+              <th class="text-right">Proteínas (g)</th>
+              <th class="text-right">Lipidios (g)</th>
+              <th class="text-right">Carboidratos (g)</th>
+              <th class="text-right">Fibra (g)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="food in diet" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 border-b">
+              <td class="text-xs">{{ food.name }}</td>
+              <td class="text-xs">{{ food.category }}</td>
+              <td class="text-right">{{ formatDecimal(food.energy_kcal, 1) }}</td>
+              <td class="text-right">{{ formatDecimal(food.protein_g, 1) }}</td>
+              <td class="text-right">{{ formatDecimal(food.lipids_g, 1) }}</td>
+              <td class="text-right">{{ formatDecimal(food.carbohydrate_g, 1) }}</td>
+              <td class="text-right">{{ formatDecimal(food.dietary_fiber_g, 1) }}</td>
+            </tr>
+            <tr>
+              <td colspan="2" class="text-right text-lg font-bold">
+                  Total
+              </td>
+              <td class="text-right font-semibold">
+                {{ formatDecimal(diet.reduce((acc, f) => {return acc + f.energy_kcal}, 0), 1) }}
+               </td>
+              <td class="text-right font-semibold">
+                {{ formatDecimal(diet.reduce((acc, f) => {return acc + f.protein_g}, 0), 1) }}
+              </td>
+              <td class="text-right font-semibold">
+                {{ formatDecimal(diet.reduce((acc, f) => {return acc + f.lipids_g}, 0), 1) }}
+              </td>
+              <td class="text-right font-semibold">
+                {{ formatDecimal(diet.reduce((acc, f) => {return acc + f.carbohydrate_g}, 0), 1) }}
+              </td>
+              <td class="text-right font-semibold">
+                {{ formatDecimal(diet.reduce((acc, f) => {return acc + f.dietary_fiber_g}, 0), 1) }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <!-- Refazer dieta -->
+        <div class="p-4">
+          <!-- TODO: utilizar 'postGeneratedDiet' -->
+          <button @click="getGeneratedDiet()" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-2 py-1 me-1 mb-1 focus:outline-none">
+            Refazer dieta
+          </button>
+        </div>
+      </div>
+
+      <!-- Gerar dieta -->
+      <div v-else class="flex justify-center">
+        <!-- TODO: utilizar 'postGeneratedDiet' -->
         <button @click="getGeneratedDiet()" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-2 py-1 me-1 mb-1 focus:outline-none">
-          Refazer dieta
+          Gerar dieta
         </button>
       </div>
-    </div>
-    <div v-else class="flex justify-center w-1/3">
-      <button @click="getGeneratedDiet()" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-2 py-1 me-1 mb-1 focus:outline-none">
-        Gerar dieta
-      </button>
     </div>
 
   </div>
--- client/src/components/Target.vue
+++ client/src/components/Target.vue
@@ -15,35 +15,35 @@ const props = defineProps({
       Valores alvo:
     </div>
 
-    <div class="flex flex-wrap space-x-2">
-      <div>
-        <label for="number-input" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Número porções:</label>
-        <input type="number" v-model="target.servings" class="bg-gray-50 max-w-32 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
+    <div class="flex flex-wrap">
+      <div class="mx-1">
+        <label for="number-input" class="block mb-1 text-xs font-medium text-gray-900 dark:text-white">Número porções:</label>
+        <input type="number" v-model="target.servings" class="bg-gray-50 max-w-24 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
       </div>
 
-      <div>
-        <label for="number-input" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Calorias:</label>
-        <input type="number" v-model="target.calories" class="bg-gray-50 max-w-32 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
+      <div class="mx-1">
+        <label for="number-input" class="block mb-1 text-xs font-medium text-gray-900 dark:text-white">Calorias:</label>
+        <input type="number" v-model="target.calories" class="bg-gray-50 max-w-24 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
       </div>
 
-      <div>
-        <label for="number-input" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Proteínas:</label>
-        <input type="number" v-model="target.proteins" class="bg-gray-50 max-w-32 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
+      <div class="mx-1">
+        <label for="number-input" class="block mb-1 text-xs font-medium text-gray-900 dark:text-white">Proteínas:</label>
+        <input type="number" v-model="target.proteins" class="bg-gray-50 max-w-24 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
       </div>
 
-      <div>
-        <label for="number-input" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Lipídios:</label>
-        <input type="number" v-model="target.lipids" class="bg-gray-50 max-w-32 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
+      <div class="mx-1">
+        <label for="number-input" class="block mb-1 text-xs font-medium text-gray-900 dark:text-white">Lipídios:</label>
+        <input type="number" v-model="target.lipids" class="bg-gray-50 max-w-24 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
       </div>
 
-      <div>
-        <label for="number-input" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Carboidratos:</label>
-        <input type="number" v-model="target.carbohydrates" class="bg-gray-50 max-w-32 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
+      <div class="mx-1">
+        <label for="number-input" class="block mb-1 text-xs font-medium text-gray-900 dark:text-white">Carboidratos:</label>
+        <input type="number" v-model="target.carbohydrates" class="bg-gray-50 max-w-24 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
       </div>
 
-      <div>
-        <label for="number-input" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Fibras:</label>
-        <input type="number" v-model="target.fibers" class="bg-gray-50 max-w-32 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
+      <div class="mx-1">
+        <label for="number-input" class="block mb-1 text-xs font-medium text-gray-900 dark:text-white">Fibras:</label>
+        <input type="number" v-model="target.fibers" class="bg-gray-50 max-w-24 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
       </div>
     </div>
 
